f2a5e9090e5b3fb9a34023bfac9441cc2f0b4839
wwer91 <46205930+wwer91@users.noreply.github.com>
2019-04-10 14:51:30 +0900

    2019.04.10
    메가박스 리뉴얼 최신버전
    -main 완료
    -고객센터 완료
    -큐레이터 완료
    -멤버십 완료

---
+++ asset/js/custom.js
@@ -0,0 +1,7 @@
+//1on1
+// $("#user_email3").change(function(){
+//   $("#user_email2").val( $("#user_email3").val() );
+// });
+$("#user_email3").change(function(){
+  $("#user_email2").val( $("#user_email3").val() );
+});
